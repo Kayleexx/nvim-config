8b3dea6cf87098bb321c27607790891e3866830b
Mitali <105365766+Kayleexx@users.noreply.github.com>
2025-11-08 19:28:06 +0530

    feat: add discord presence, rainbow indents, and smooth scrolling

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,5 +1,4 @@
 return {
-  -- Install and configure Kanagawa
   {
     "rebelot/kanagawa.nvim",
     name = "kanagawa",
@@ -9,22 +8,16 @@ return {
       overrides = function(colors)
         local theme = colors.theme
         return {
-          -- Make floating windows transparent/darker
           NormalFloat = { bg = "none" },
           FloatBorder = { bg = "none" },
 
-          -- Make plugin manager windows (Lazy, Mason) match better
           LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
           MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-
-          -- Optional: Make line numbers cooler (grey instead of default)
           LineNr = { fg = theme.ui.bg_p2 },
         }
       end,
     },
   },
-
-  -- Tell LazyVim to use it
   {
     "LazyVim/LazyVim",
     opts = {
